2178edb3364027cecb641e9166c0df6f36da9ae8
ilyasse-CH7 <ilyassechatir7@gmail.com>
2026-05-04 09:53:05 +0100

    TP2

---
+++ TP3/script.js
@@ -0,0 +1,26 @@
+class Personne{
+
+    #nom;
+    #prenom ;
+    #age;
+    constructor(nom , prenom , age){
+        this.#nom = nom ;
+        this.#prenom = pernom ;
+        this.#age = age ;
+    }
+
+    get nom() {
+        return this.#nom;
+    }
+
+    get prenom() {
+        return this.#prenom;
+    }
+
+    get age() {
+        return this.#age;
+    }
+
+
+
+}
